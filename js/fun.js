d83528e90ea62c1f37a78304c17e796285ce877a
Paul <paulozyzniewski@bigfootdigital.co.uk>
2020-02-06 14:22:06 +0000

    Add .line's stroke animation

--- js/fun.js
+++ js/fun.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
   var pinPoints = document.querySelectorAll(".point");
 
+  function removeAllActiveClasses(elementsList) {
+    elementsList.forEach(function(point) {
+      point.classList.remove("active");
+    });
+  }
+
   pinPoints.forEach(function(point) {
     point.addEventListener("click", function() {
-      pinPoints.forEach(function(point) {
-        point.classList.remove("active");
-      });
-
-      this.classList.toggle("active");
+      removeAllActiveClasses(pinPoints);
+      this.classList.add("active");
     });
   });
 });
