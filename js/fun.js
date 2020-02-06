70be7aced0f0dad597eb95833d3549aa83547398
Paul <paulozyzniewski@bigfootdigital.co.uk>
2020-02-06 12:29:47 +0000

    Add event listeners for pin points

--- js/fun.js
+++ js/fun.js
@@ -1,1 +1,13 @@
-console.log("elo");
+document.addEventListener("DOMContentLoaded", function() {
+  var pinPoints = document.querySelectorAll(".point");
+
+  pinPoints.forEach(function(point) {
+    point.addEventListener("click", function() {
+      pinPoints.forEach(function(point) {
+        point.classList.remove("active");
+      });
+
+      this.classList.toggle("active");
+    });
+  });
+});
