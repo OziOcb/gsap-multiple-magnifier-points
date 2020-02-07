eb327834e47992b3ce33a5d6c138a2d6e2481331
Paul <paulozyzniewski@bigfootdigital.co.uk>
2020-02-07 15:02:36 +0000

    Create fun-copy.js file and start from scratch

--- js/fun.js
+++ js/fun.js
@@ -1,47 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var points = document.querySelectorAll(".point");
-  var targetParent = null;
-  var panel = {
-    point: null,
-    line: null,
-    image: null,
-    title: null,
-    desc: null
-  };
+var points = document.querySelectorAll(".point");
+
+points.forEach(function(point, index) {
+  point.addEventListener("click", onClick);
+});
+
+function onClick(e) {
+  var active = document.querySelector(".activated");
+
+  if (active) {
+    TweenMax.to(".activated", 0.75, { fill: "transparent" });
+  }
 
   points.forEach(function(point) {
-    targetParent = point.parentNode;
-    panel = {
-      point: targetParent.querySelectorAll(".point"),
-      line: targetParent.querySelector(".line"),
-      image: targetParent.querySelector(".image"),
-      title: targetParent.querySelector(".title"),
-      desc: targetParent.querySelector(".description")
-    };
-
-    const tl = gsap.timeline({ paused: false });
-    tl.to(panel.point, 0.3, { scale: 0.9, transformOrigin: "50% 50%" })
-      .to(panel.point, 0.2, { scale: 1.2 })
-      .to(panel.point, 0.2, { scale: 1 })
-      .to(panel.line, 1, { attr: { "stroke-dashoffset": 0 }, opacity: 1 }, "-=.4")
-      .to(
-        panel.image,
-        2,
-        {
-          attr: { "stroke-dashoffset": 0 },
-          opacity: 1,
-          ease: "power4"
-        },
-        "-=0.5"
-      )
-      .reverse();
-
-    point.addEventListener("click", function() {
-      if (tl.reversed()) {
-        tl.play().timeScale(1);
-      } else {
-        tl.timeScale(4).reverse();
-      }
-    });
+    point.classList.remove("activated");
   });
-});
+
+  TweenMax.to(e.target, 1, { fill: "red" });
+  e.target.classList.add("activated");
+}
