11f49bd9a3b1bf33f72bcbd53fef50a572eca8dd
Paul <paulozyzniewski@bigfootdigital.co.uk>
2020-02-07 10:59:36 +0000

    Add animation to all elements

--- js/fun.js
+++ js/fun.js
@@ -1,37 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
   var points = document.querySelectorAll(".point");
   var targetParent = null;
-  var point = null;
-  var line = null;
-  var image = null;
-  var title = null;
-  var desc = null;
-
-  const tl = gsap.timeline();
+  var panel = {
+    point: null,
+    line: null,
+    image: null,
+    title: null,
+    desc: null
+  };
 
   points.forEach(function(point) {
-    point.addEventListener("click", function(e) {
-      targetParent = e.target.parentNode;
-      point = targetParent.querySelectorAll(".point");
-      line = targetParent.querySelector(".line");
-      image = targetParent.querySelector(".image");
-      title = targetParent.querySelector(".title");
-      desc = targetParent.querySelector(".description");
+    targetParent = point.parentNode;
+    panel = {
+      point: targetParent.querySelectorAll(".point"),
+      line: targetParent.querySelector(".line"),
+      image: targetParent.querySelector(".image"),
+      title: targetParent.querySelector(".title"),
+      desc: targetParent.querySelector(".description")
+    };
 
-      tl.to(point, 0.3, { scale: 0.9, transformOrigin: "50% 50%" })
-        .to(point, 0.2, { scale: 1.2 })
-        .to(point, 0.2, { scale: 1 })
-        .to(line, 1, { attr: { "stroke-dashoffset": 0 }, opacity: 1 }, "-=.4")
-        .to(
-          image,
-          2,
-          {
-            attr: { "stroke-dashoffset": 0 },
-            opacity: 1,
-            ease: "power4"
-          },
-          "-=0.5"
-        );
+    const tl = gsap.timeline({ paused: false });
+    tl.to(panel.point, 0.3, { scale: 0.9, transformOrigin: "50% 50%" })
+      .to(panel.point, 0.2, { scale: 1.2 })
+      .to(panel.point, 0.2, { scale: 1 })
+      .to(panel.line, 1, { attr: { "stroke-dashoffset": 0 }, opacity: 1 }, "-=.4")
+      .to(
+        panel.image,
+        2,
+        {
+          attr: { "stroke-dashoffset": 0 },
+          opacity: 1,
+          ease: "power4"
+        },
+        "-=0.5"
+      )
+      .reverse();
+
+    point.addEventListener("click", function(e) {
+      tl.reversed(!tl.reversed());
     });
   });
 });
