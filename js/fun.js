86f441ccd2ebbdc79abc9da213c0d8a533019f28
Paul <paulozyzniewski@bigfootdigital.co.uk>
2020-02-07 10:46:46 +0000

    Add basic gsap.timeline

--- js/fun.js
+++ js/fun.js
@@ -1,16 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var pinPoints = document.querySelectorAll(".point");
+  var points = document.querySelectorAll(".point");
+  var targetParent = null;
+  var point = null;
+  var line = null;
+  var image = null;
+  var title = null;
+  var desc = null;
 
-  function removeAllActiveClasses(elementsList) {
-    elementsList.forEach(function(point) {
-      point.classList.remove("active");
-    });
-  }
+  const tl = gsap.timeline();
 
-  pinPoints.forEach(function(point) {
-    point.addEventListener("click", function() {
-      removeAllActiveClasses(pinPoints);
-      this.classList.add("active");
+  points.forEach(function(point) {
+    point.addEventListener("click", function(e) {
+      targetParent = e.target.parentNode;
+      point = targetParent.querySelectorAll(".point");
+      line = targetParent.querySelector(".line");
+      image = targetParent.querySelector(".image");
+      title = targetParent.querySelector(".title");
+      desc = targetParent.querySelector(".description");
+
+      tl.to(point, 0.3, { scale: 0.9, transformOrigin: "50% 50%" })
+        .to(point, 0.2, { scale: 1.2 })
+        .to(point, 0.2, { scale: 1 })
+        .to(line, 1, { attr: { "stroke-dashoffset": 0 }, opacity: 1 }, "-=.4")
+        .to(
+          image,
+          2,
+          {
+            attr: { "stroke-dashoffset": 0 },
+            opacity: 1,
+            ease: "power4"
+          },
+          "-=0.5"
+        );
     });
   });
 });
